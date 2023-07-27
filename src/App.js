cb5e2d3c08af8b8ae7eb0544d2ed7e4cda3aa464
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-07-27 13:42:09 -0400

    added basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import React from "react";
+// import Header from "./components/Header";
+import NavigationBar from "./components/NavigationBar";
+// import Footer from "./components/Footer";
+// import Project from "./components/Project";
 
 function App() {
-  return <div>Stuff goes here</div>;
+  return (
+    <div>
+      <NavigationBar />
+      {/* <Header />
+      <Project />
+      <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function NavigationBar() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Myro Joy Lee</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="">About Me</Nav.Link>
+            <NavDropdown title="Portfolio" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="">Project 1</NavDropdown.Item>
+              <NavDropdown.Item href="">Project 2</NavDropdown.Item>
+              <NavDropdown.Item href="">Project 3</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="">Contact</Nav.Link>
+            <Nav.Link href="">Resume</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
