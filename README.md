3e67e8aeecc812830604599fa1afdeed6d09bed7
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-07-29 01:11:51 -0400

    added form validation for contact page

--- README.md
+++ README.md
@@ -17,16 +17,16 @@ WHEN I load the portfolio
 - [x] THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
 - [x] WHEN I load the portfolio the first time
 - [x] THEN the About Me title and section are selected by default
-- [ ] WHEN I am presented with the About Me section
-- [ ] THEN I see a recent photo or avatar of the developer and a short bio about them
+- [x] WHEN I am presented with the About Me section
+- [x] THEN I see a recent photo or avatar of the developer and a short bio about them
 - [ ] WHEN I am presented with the Portfolio section
 - [ ] THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
-- [ ] WHEN I am presented with the Contact section
-- [ ] THEN I see a contact form with fields for a name, an email address, and a message
-- [ ] WHEN I move my cursor out of one of the form fields without entering text
-- [ ] THEN I receive a notification that this field is required
-- [ ] WHEN I enter text into the email address field
-- [ ] THEN I receive a notification if I have entered an invalid email address
+- [x] WHEN I am presented with the Contact section
+- [x] THEN I see a contact form with fields for a name, an email address, and a message
+- [x] WHEN I move my cursor out of one of the form fields without entering text
+- [x] THEN I receive a notification that this field is required
+- [x] WHEN I enter text into the email address field
+- [x] THEN I receive a notification if I have entered an invalid email address
 - [ ] WHEN I am presented with the Resume section
 - [ ] THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
 - [ ] WHEN I view the footer
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,28 +1,150 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../../styles/Contact.css";
 
-export default function Contact() {
+import { validateEmail } from "../../utils/helpers";
+
+function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [touched, setTouched] = useState("");
+
+  const handleInputChange = (e) => {
+    setErrorMessage("");
+    const { target } = e;
+    const inputType = target.id;
+    const inputValue = target.value;
+
+    if (inputType === "formBasicText") {
+      setName(inputValue);
+    } else if (inputType === "formBasicEmail") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+
+    // if (touched) {
+    //   setErrorMessage("Name is required");
+    // }
+    // if (!message) {
+    //   setErrorMessage("Message is required");
+    // }
+
+    // if (!email) {
+    //   setErrorMessage("Email is required");
+    //   return;
+    // } else if (email !== "" && !validateEmail(email)) {
+    //   setErrorMessage("Email is invalid");
+    // } else {
+    //   setErrorMessage("");
+    // }
+  };
+
+  const handleName = (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    if (!name) {
+      setErrorMessage("Name required");
+      return;
+    } else {
+      if (!email) {
+        setErrorMessage("Email required");
+        return;
+      } else if (!validateEmail(email)) {
+        setErrorMessage("Email is invalid");
+        return;
+      } else if (!message) {
+        setErrorMessage("Message required");
+        return;
+      }
+    }
+  };
+
+  const handleEmail = (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    if (!email) {
+      setErrorMessage("Email required");
+      return;
+    } else {
+      if (!validateEmail(email)) {
+        setErrorMessage("Email is invalid");
+        return;
+      } else {
+        if (!name) {
+          setErrorMessage("Name required");
+          return;
+        } else if (!message) {
+          setErrorMessage("Message required");
+          return;
+        }
+      }
+    }
+  };
+
+  const handleMessage = (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    if (!message) {
+      setErrorMessage("Message required");
+      return;
+    } else {
+      if (!name) {
+        setErrorMessage("Name required");
+        return;
+      } else if (!email) {
+        setErrorMessage("Email required");
+        return;
+      } else if (!validateEmail(email)) {
+        setErrorMessage("Email is invalid");
+        return;
+      }
+    }
+  };
+
   return (
     <section className="contact-detail">
       <h1>Let's Chat!</h1>
       <article className="contact-form-wrapper">
         <Form>
+          <Form.Group className="mb-3" controlId="formBasicText">
+            <Form.Label>Name:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter your name"
+              onChange={handleInputChange}
+              onBlur={handleName}
+              value={name}
+            />
+          </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Label>Email address:</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              onChange={handleInputChange}
+              onBlur={handleEmail}
+              value={email}
+            />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+          <Form.Group className="mb-3" controlId="formControlTextarea">
+            <Form.Label>
+              Send me a message and I will get back to you as soon as I can:
+            </Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Enter message"
+              rows="5"
+              onChange={handleInputChange}
+              onBlur={handleMessage}
+              value={message}
+            />
+            <Form.Text className="text-muted">{errorMessage}</Form.Text>
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
@@ -32,3 +154,5 @@ export default function Contact() {
     </section>
   );
 }
+
+export default Contact;
