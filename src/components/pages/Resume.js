44337f4c8082a1f1d3dd752460373f435dbf9074
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-07-29 03:54:10 -0400

    added pdf to resume section and added a list

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "../../styles/Resume.css";
+import ResumeMJLeePDF from "../../assets/Resume-bw-mjlee-2.pdf";
 
 export default function Resume() {
   return (
     <section className="resume-detail">
       <h1>Resume</h1>
-      <p>Resume here</p>
+      <a
+        href={ResumeMJLeePDF}
+        download="ResumeMJLeePDF"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Download PDF
+      </a>
+      <article className="resume-list">
+        <h4>Front-End Proficiencies</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>Bulma</li>
+        </ul>
+      </article>
     </section>
   );
 }
